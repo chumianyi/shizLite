8742b0e81d4d2b9d25a9fb1528f93c501b08676b
chumianyi <chumian@example.com>
2026-08-16 09:43:55 +0800

    fix: settings fragment wrap instead of extend (final class)

--- manager/src/main/java/moe/shizuku/manager/nav/SettingsFragment.kt
+++ manager/src/main/java/moe/shizuku/manager/nav/SettingsFragment.kt
@@ -1,5 +1,28 @@
 package moe.shizuku.manager.nav
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
 import moe.shizuku.manager.settings.SettingsFragment as OriginalSettingsFragment
 
-class SettingsFragment : OriginalSettingsFragment()
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val container = FrameLayout(requireContext()).apply {
+            id = View.generateViewId()
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        }
+        if (savedInstanceState == null) {
+            childFragmentManager.beginTransaction()
+                .replace(container.id, OriginalSettingsFragment())
+                .commit()
+        }
+        return container
+    }
+}
